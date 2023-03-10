f64fdd88c6d6f5366ab153b2862e76d08070440d
redundant4u <physiogel@pusan.ac.kr>
2023-03-10 20:19:06 +0900

    fix: 마지막으로 연결된 웹 터미널에만 입력이 버그 수정

--- src/terminal/terminal.gateway.ts
+++ src/terminal/terminal.gateway.ts
@@ -1,14 +1,7 @@
 import { Logger } from '@nestjs/common';
-import {
-    ConnectedSocket,
-    MessageBody,
-    OnGatewayConnection,
-    OnGatewayDisconnect,
-    SubscribeMessage,
-    WebSocketGateway,
-} from '@nestjs/websockets';
+import { ConnectedSocket, OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
+import { IPty, spawn } from 'node-pty';
 import { Socket } from 'socket.io';
-import { Pty } from './pty';
 
 @WebSocketGateway({
     namespace: 'terminal',
@@ -16,23 +9,41 @@ import { Pty } from './pty';
     transports: ['websocket'],
 })
 export default class TerminalGateway implements OnGatewayConnection, OnGatewayDisconnect {
-    pty: Pty;
+    private ptys: Map<string, IPty> = new Map();
 
     handleConnection(@ConnectedSocket() socket: Socket) {
         Logger.log(`connect: ${socket.id}`);
+
+        const pty = spawn('docker', ['exec', '-it', 'terminal', 'bash'], {
+            name: 'xterm-color',
+            cwd: process.env.HOME,
+        });
+
+        pty.onData((data) => {
+            socket.emit('output', data);
+        });
+
+        const { id } = socket;
+        this.ptys.set(id, pty);
+
+        socket.on('input', (data) => {
+            const pty = this.ptys.get(id);
+
+            if (pty) {
+                pty.write(data);
+            }
+        });
     }
 
     handleDisconnect(@ConnectedSocket() socket: Socket) {
         Logger.log(`disconnect: ${socket.id}`);
-    }
 
-    @SubscribeMessage('init')
-    init(@ConnectedSocket() socket: Socket) {
-        this.pty = new Pty(socket);
-    }
+        const { id } = socket;
+        const pty = this.ptys.get(id);
 
-    @SubscribeMessage('input')
-    write(_, @MessageBody() data: string) {
-        this.pty.write(data);
+        if (pty) {
+            pty.kill();
+            this.ptys.delete(id);
+        }
     }
 }
--- src/terminal/terminal.module.ts
+++ src/terminal/terminal.module.ts
@@ -1,4 +1,4 @@
-import { Logger, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import TerminalGateway from './terminal.gateway';
 
 @Module({
