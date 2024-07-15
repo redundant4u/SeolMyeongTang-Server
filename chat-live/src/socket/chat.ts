a4a5c302a5a6c499a9cf00eceddcc1908546b1ad
redundant4u <physiogel@pusan.ac.kr>
2024-07-15 14:01:21 +0900

    fix: chat handler 로직 수정

--- chat-live/src/socket/chat.ts
+++ chat-live/src/socket/chat.ts
@@ -1,22 +1,40 @@
 import { Socket } from "socket.io";
 import { pubClient } from "../utils/redis";
-import { query } from "../utils/db";
+import { wrapChatQuery } from "../utils/wrap";
 
-const chatHandlers = (io: Socket) => {
-    const createChat = async (message: any) => {
-        const chatCount = await pubClient.rPush("chat", message);
+type Chat = {
+    id: number;
+    content: string;
+    createdAt: string;
+};
+
+const chatHandlers = (socket: Socket) => {
+    const createChat = async (chat: string) => {
+        /*
+        const chatCount = await pubClient.rPush("chat", chat);
 
         if (chatCount >= 10) {
-            const messages = await pubClient.lRange("chat", 0, 9);
-            const values = messages.map((message) => message);
+            const chats = await pubClient.lRange("chat", 0, 9);
+            const values = chats.map((chat) => chat);
             await query(
                 `INSERT INTO chat (content) VALUES ($1), ($2), ($3), ($4), ($5), ($6), ($7), ($8), ($9), ($10)`,
                 values
             );
             await pubClient.lPopCount("chat", 10);
         }
+        */
 
-        io.broadcast.emit("message", message);
+        await pubClient.rPush("chat", chat);
+        const mqChat = await pubClient.lRange("chat", 0, 1).then((chats) => chats[0]);
+        await pubClient.lPopCount("chat", 1);
+
+        const newChat = await wrapChatQuery<Chat>("INSERT INTO chats (content) VALUES ($1) RETURNING *", [mqChat]).then(
+            (result) => result[0]
+        );
+
+        const data = { content: newChat.content, createdAt: newChat.createdAt };
+        socket.emit("chat", data);
+        socket.broadcast.emit("chat", data);
     };
 
     return {
