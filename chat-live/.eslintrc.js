b9535794eb994d2b6352d06d2418aa278faaba06
redundant4u <physiogel@pusan.ac.kr>
2024-07-16 17:47:17 +0900

    chore: eslint 버전 수정 및 ignore 파일 추가

--- chat-live/.eslintrc.js
+++ chat-live/.eslintrc.js
@@ -14,9 +14,11 @@ module.exports = {
     },
     ignorePatterns: [".eslintrc.js"],
     rules: {
+        "no-unused-vars": "off",
         "@typescript-eslint/interface-name-prefix": "off",
         "@typescript-eslint/explicit-function-return-type": "off",
         "@typescript-eslint/explicit-module-boundary-types": "off",
-        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-explicit-any": "warn",
+        "@typescript-eslint/no-unused-vars": "warn",
     },
 };
