ee7b995961ca7c8e2cd7b4f51d6368a55016556e
redundant4u <physiogel@pusan.ac.kr>
2024-07-23 17:10:47 +0900

    chore: port, chat_ws_path 환경변수 추가

--- chat-live/src/index.ts
+++ chat-live/src/index.ts
@@ -1,13 +1,14 @@
 import { createSocket, onConnection } from "./socket";
 import { redisConnect } from "./utils/redis";
 import { pgConnect } from "./utils/db";
+import env from "./utils/env";
 
 const boot = async () => {
     await pgConnect();
     await redisConnect();
 
-    const io = createSocket();
-    io.listen(8080).of("/chat").on("connection", onConnection);
+    const io = createSocket().path(env.CHAT_WS_PATH);
+    io.listen(env.PORT).of("/chat").on("connection", onConnection);
 };
 
 boot().catch((err) => {
--- chat-live/src/utils/env.ts
+++ chat-live/src/utils/env.ts
@@ -5,13 +5,15 @@ dotenv.config();
 
 const envSchema = z.object({
     NODE_ENV: z.enum(["production", "development"]),
+    PORT: z.string().transform((val) => parseInt(val, 10)),
+    CHAT_WS_PATH: z.string(),
     DB_HOST: z.string(),
     DB_USER: z.string(),
     DB_PASSWORD: z.string(),
     DB_NAME: z.string(),
-    DB_PORT: z.number(),
+    DB_PORT: z.string().transform((val) => parseInt(val, 10)),
     REDIS_HOST: z.string(),
-    REDIS_PORT: z.number(),
+    REDIS_PORT: z.string().transform((val) => parseInt(val, 10)),
 });
 
 const env = envSchema.safeParse(process.env);
