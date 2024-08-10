8cb63c720371f7651ed1588efc05ad50c9a53c2d
redundant4u <physiogel@pusan.ac.kr>
2024-08-11 08:13:42 +0900

    fix: cors option 수정

--- chat-live/src/socket/index.ts
+++ chat-live/src/socket/index.ts
@@ -6,7 +6,11 @@ import { wrapSocket } from "../utils/wrap";
 
 export const createSocket = () => {
     return new Server({
-        cors: {},
+        cors: {
+            origin: ["https://redundant4u.com"],
+            methods: ["GET", "OPTIONS"],
+            allowedHeaders: ["Origin", "Authorization", "Content-Type"],
+        },
         transports: ["websocket"],
         httpCompression: true,
         adapter: createAdapter(pubClient, subClient),
@@ -14,6 +18,7 @@ export const createSocket = () => {
 };
 
 export const onConnection = (socket: Socket) => {
+    console.log(socket.id);
     const chat = chatHandlers(socket);
     socket.on("create", wrapSocket(chat.createChat, socket));
 };
